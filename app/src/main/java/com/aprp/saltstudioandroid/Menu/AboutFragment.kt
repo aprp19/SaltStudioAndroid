2b097fe280586fe6ee2cbedfc2fe569555811d3e
Angga P <kierska19@gmail.com>
2020-07-10 21:52:36 +0700

    Adding about page

--- app/src/main/java/com/aprp/saltstudioandroid/Menu/AboutFragment.kt
+++ app/src/main/java/com/aprp/saltstudioandroid/Menu/AboutFragment.kt
@@ -1,11 +1,17 @@
 package com.aprp.saltstudioandroid.Menu
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.view.Gravity
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.fragment.app.Fragment
 import com.aprp.saltstudioandroid.R
+import mehdi.sakout.aboutpage.AboutPage
+import mehdi.sakout.aboutpage.Element
+import java.util.*
+
 
 class AboutFragment : Fragment() {
 
@@ -14,7 +20,33 @@ class AboutFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_about, container, false)
+
+        return AboutPage(context)
+            .isRTL(false)
+            .setImage(R.drawable.ic_saltstudio)
+            .setDescription(getString(R.string.about_desc))
+            .addGroup("Contact Us")
+            .addEmail("SaltGameStudio@Gmail.com","Email")
+            .addItem(Copyright())
+            .create()
+//        return inflater.inflate(R.layout.fragment_about, container, false)
+    }
+
+    fun Copyright(): Element {
+        val CopyrightsElement = Element()
+        val copyrights = String.format(
+            getString(R.string.about_copyrights),
+            Calendar.getInstance().get(Calendar.YEAR)
+        )
+        CopyrightsElement.setTitle(copyrights)
+        CopyrightsElement.setAutoApplyIconTint(true)
+        CopyrightsElement.setIconTint(mehdi.sakout.aboutpage.R.color.about_item_icon_color);
+        CopyrightsElement.setIconNightTint(android.R.color.white)
+        CopyrightsElement.setGravity(Gravity.CENTER)
+        CopyrightsElement.setOnClickListener(View.OnClickListener {
+            Toast.makeText(context,copyrights,Toast.LENGTH_SHORT).show()
+        })
+        return CopyrightsElement
     }
 
     companion object {
