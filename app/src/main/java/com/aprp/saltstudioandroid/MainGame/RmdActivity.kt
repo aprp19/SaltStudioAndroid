0d2b6d8e4ea8186cfcde43d198bdd3f1112ce08f
Angga P <kierska19@gmail.com>
2020-07-20 13:17:44 +0700

    Adding Rumah Makan Delivery Page and its content

---
+++ app/src/main/java/com/aprp/saltstudioandroid/MainGame/RmdActivity.kt
@@ -0,0 +1,113 @@
+package com.aprp.saltstudioandroid.MainGame
+
+import android.Manifest
+import android.app.ProgressDialog
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.os.Build
+import android.os.Bundle
+import android.os.Environment
+import android.util.Log
+import android.view.View
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.fragment.app.FragmentActivity
+import com.aprp.saltstudioandroid.MainActivity
+import com.aprp.saltstudioandroid.R
+import com.google.firebase.ktx.Firebase
+import com.google.firebase.storage.FirebaseStorage
+import com.google.firebase.storage.ktx.storage
+import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.views.YouTubePlayerView
+import kotlinx.android.synthetic.main.activity_rmd.*
+import java.io.File
+
+
+class RmdActivity : AppCompatActivity() {
+
+    lateinit var storage: FirebaseStorage
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_rmd)
+
+        storage = Firebase.storage
+
+        rmd_bckbtn.setOnClickListener {
+            startActivity(Intent(this,MainActivity::class.java))
+        }
+
+        val youTubePlayerView: YouTubePlayerView = findViewById(R.id.rmd_trailer)
+        lifecycle.addObserver(youTubePlayerView)
+
+        rmd_psdl.setOnClickListener {
+            Toast.makeText(this,"Not Yet Available",Toast.LENGTH_SHORT).show()
+        }
+
+        rmd_mldl.setOnClickListener {
+            isStoragePermissionGranted()
+        }
+    }
+
+    fun isStoragePermissionGranted(): Boolean {
+        return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                == PackageManager.PERMISSION_GRANTED
+            ) {
+                Log.v("firebase", "Permission is granted")
+                downloadFile()
+                true
+            } else {
+                Log.v("firebase", "Permission is revoked")
+                ActivityCompat.requestPermissions(
+                    this,
+                    arrayOf(Manifest.permission.WRITE_EXTERNAL_STORAGE),
+                    1
+                )
+                false
+            }
+        } else { //permission is automatically granted on sdk<23 upon installation
+            Log.v("firebase", "Permission is granted")
+            downloadFile()
+            true
+        }
+    }
+
+    private fun downloadFile() {
+        val storage = FirebaseStorage.getInstance()
+        val storageRef =
+            storage.getReferenceFromUrl("https://firebasestorage.googleapis.com/v0/b/saltstudioandroid.appspot.com/o/game_utama%2FRMDv0.1.apk?alt=media&token=4e54e46b-c806-41d1-b483-e30bc8c789ab")
+        val pd = ProgressDialog(this)
+        pd.setTitle("RMDv0.1.apk")
+        pd.setMessage("Downloading Please Wait!")
+        pd.isIndeterminate = true
+        pd.setProgressStyle(ProgressDialog.STYLE_SPINNER)
+        pd.show()
+        val rootPath =
+            File(Environment.getExternalStorageDirectory(),"SaltStudioAndroid")
+        if (!rootPath.exists()) {
+            rootPath.mkdirs()
+        }
+        val localFile = File(rootPath, "RMDv0.1.apk")
+        storageRef.getFile(localFile).addOnSuccessListener {
+            Log.e(
+                "firebase ",
+                ";local tem file created  created $localFile"
+            )
+            if (!pd.isShowing){
+                return@addOnSuccessListener
+            }
+            if (localFile.canRead()) {
+                pd.dismiss()
+            }
+            Toast.makeText(applicationContext, "Download Completed", Toast.LENGTH_SHORT).show()
+            Toast.makeText(applicationContext, "Internal Storage/SaltStudioAndroid/RMDv0.1.apk", Toast.LENGTH_LONG).show()
+        }.addOnFailureListener { exception ->
+            Log.e(
+                "firebase ",
+                ";local tem file not created $exception"
+            )
+            Toast.makeText(applicationContext, "Download Incompleted", Toast.LENGTH_LONG).show()
+        }
+    }
+}
--- app/src/main/java/com/aprp/saltstudioandroid/Menu/HomeFragment.kt
+++ app/src/main/java/com/aprp/saltstudioandroid/Menu/HomeFragment.kt
@@ -1,11 +1,14 @@
 package com.aprp.saltstudioandroid.Menu
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.aprp.saltstudioandroid.MainGame.RmdActivity
 import com.aprp.saltstudioandroid.R
+import kotlinx.android.synthetic.main.fragment_home.*
 
 class HomeFragment : Fragment() {
 
@@ -15,6 +18,14 @@ class HomeFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_home, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        home_checkbtn.setOnClickListener {
+            startActivity(Intent(context,RmdActivity::class.java))
+        }
     }
     companion object{
         fun newInstance(): HomeFragment{
