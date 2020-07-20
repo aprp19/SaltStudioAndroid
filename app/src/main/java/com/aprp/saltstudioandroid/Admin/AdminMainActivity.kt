08409a6c09a6304c216f4c2d4571c38bdcc3fe8e
Angga P <kierska19@gmail.com>
2020-07-20 13:20:39 +0700

    Completing App

--- app/src/main/java/com/aprp/saltstudioandroid/Admin/AdminMainActivity.kt
+++ app/src/main/java/com/aprp/saltstudioandroid/Admin/AdminMainActivity.kt
@@ -3,6 +3,7 @@ package com.aprp.saltstudioandroid.Admin
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.aprp.saltstudioandroid.Menu.ShopFragment
 import com.aprp.saltstudioandroid.R
 import kotlinx.android.synthetic.main.activity_admin_main.*
 
@@ -11,6 +12,7 @@ class AdminMainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_admin_main)
 
+        admin_bckbtn.setOnClickListener { startActivity(Intent(this,ShopFragment::class.java)) }
         admin_addbtn.setOnClickListener { startActivity(Intent(this,AddActivity::class.java)) }
     }
 }
